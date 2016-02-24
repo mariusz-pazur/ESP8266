cbffb0d67e70f1ed1255302fb8b6daa2525ee9ec
mariusz-pazur <mariusz.pazur@gmail.com>
2016-02-24 22:42:37 +0100

    HomeAtion_RemoteDHT - test 4 czujników

--- HomeAtion/homeation_remotedht_clientonly/homeation_remotedht_clientonly.ino
+++ HomeAtion/homeation_remotedht_clientonly/homeation_remotedht_clientonly.ino
@@ -4,7 +4,7 @@
 #include <WiFiManager.h>         
 #include <DHT.h>
 
-//#define DEBUG   //If you comment this line, the DPRINT & DPRINTLN lines are defined as blank.
+#define DEBUG   //If you comment this line, the DPRINT & DPRINTLN lines are defined as blank.
 #ifdef DEBUG    //Macros are usually in all capital letters.
 #define DPRINT(...)    Serial.print(__VA_ARGS__)     //DPRINT is a macro, debug print
 #define DPRINTLN(...)  Serial.println(__VA_ARGS__)   //DPRINTLN is a macro, debug print with new line
@@ -14,18 +14,24 @@
 #endif
 
 char thingspeakApiUrl[] = "api.thingspeak.com";
-char thingspeakParameters[61] = "/update?api_key=5C934TN5I8BRTLZN&field1=%s&field2=%s";
-//const long thingspeakRequestInterval = 60000;
-//unsigned long thingspeakPreviousMillis = 0;
+char thingspeakParameters[] = "/update?api_key=OL9BLT52MX7C5PZ6&field1=%s&field2=%s&field3=%s&field4=%s&field5=%s&field6=%s&field7=%s&field8=%s";
 
 WiFiManager wifiManager;
 bool shouldSaveConfig = false;
 
-DHT dht(2, DHT22, 11); // 11 works fine for ESP8266
-char str_temp[6];
-char str_humid[6];
-//unsigned long dhtPreviousMillis = 0;        // will store last temp was read
-//const long dhtSensorInterval = 30000;              // interval at which to read sensor
+DHT dht4(D4, DHT22, 11); // 11 works fine for ESP8266
+DHT dht3(D3, DHT22, 11);
+DHT dht2(D2, DHT22, 11);
+DHT dht1(D1, DHT22, 11);
+
+char str_temp1[6];
+char str_humid1[6];
+char str_temp2[6];
+char str_humid2[6];
+char str_temp3[6];
+char str_humid3[6];
+char str_temp4[6];
+char str_humid4[6];
 
 const unsigned long sleepIntervalInMicro = 55 * 1000000;
 //callback notifying us of the need to save config
@@ -36,30 +42,62 @@ void saveConfigCallback()
 }
 
 void gettemperature()
-{
-	//unsigned long currentMillis = millis();
-	//if (currentMillis - dhtPreviousMillis >= dhtSensorInterval)
-	//{
-	//dhtPreviousMillis = currentMillis;
-	float h = dht.readHumidity();          // Read humidity (percent)
-	float t = dht.readTemperature();     // Read temperature as Fahrenheit
-	if (isnan(h) || isnan(t))
-	{
-		DPRINTLN("Failed to read from DHT sensor!");
-		return;
-	}
-	DPRINTLN("DHT sensor read");
-	dtostrf(t, 4, 2, str_temp);
-	dtostrf(h, 4, 2, str_humid);
-	//}
+{	
+	float h = dht1.readHumidity();          // Read humidity (percent)
+  float t = dht1.readTemperature();     // Read temperature as Fahrenheit
+  if (isnan(h) || isnan(t))
+  {
+    DPRINTLN("Failed to read from DHT1 sensor!");    
+  }
+  else
+  {
+    DPRINTLN("DHT1 sensor read");
+    dtostrf(t, 4, 2, str_temp1);
+    dtostrf(h, 4, 2, str_humid1);
+  }
+
+  h = dht2.readHumidity();          // Read humidity (percent)
+  t = dht2.readTemperature();     // Read temperature as Fahrenheit
+  if (isnan(h) || isnan(t))
+  {
+    DPRINTLN("Failed to read from DHT2 sensor!");    
+  }
+  else
+  {
+    DPRINTLN("DHT2 sensor read");
+    dtostrf(t, 4, 2, str_temp2);
+    dtostrf(h, 4, 2, str_humid2);
+  }
+
+  h = dht3.readHumidity();          // Read humidity (percent)
+  t = dht3.readTemperature();     // Read temperature as Fahrenheit
+  if (isnan(h) || isnan(t))
+  {
+    DPRINTLN("Failed to read from DHT3 sensor!");    
+  }
+  else
+  {
+    DPRINTLN("DHT3 sensor read");
+    dtostrf(t, 4, 2, str_temp3);
+    dtostrf(h, 4, 2, str_humid3);
+  }
+
+  h = dht4.readHumidity();          // Read humidity (percent)
+  t = dht4.readTemperature();     // Read temperature as Fahrenheit
+  if (isnan(h) || isnan(t))
+  {
+    DPRINTLN("Failed to read from DHT4 sensor!");    
+  }
+  else
+  {
+    DPRINTLN("DHT4 sensor read");
+    dtostrf(t, 4, 2, str_temp4);
+    dtostrf(h, 4, 2, str_humid4);
+  }
 }
 
 void sendThingspeakRequest()
 {
-	//unsigned long currentMillis = millis();
-	//if (currentMillis - thingspeakPreviousMillis >= thingspeakRequestInterval)
-	//{
-	//thingspeakPreviousMillis = currentMillis;
 	WiFiClient client;
 	const int httpPort = 80;
 	if (!client.connect(thingspeakApiUrl, httpPort))
@@ -68,8 +106,8 @@ void sendThingspeakRequest()
 		return;
 	}
 	// We now create a URI for the request
-	char parametersBuffer[61];
-	sprintf(parametersBuffer, thingspeakParameters, str_temp, str_humid);
+	char parametersBuffer[145];
+	sprintf(parametersBuffer, thingspeakParameters, str_temp1, str_humid1 , str_temp2, str_humid2, str_temp3, str_humid3, str_temp4, str_humid4);
 	String parameters(parametersBuffer);
 	String url(thingspeakApiUrl);
 	DPRINT("Requesting URL: ");
@@ -96,15 +134,18 @@ void sendThingspeakRequest()
 	}
 	DPRINTLN();
 	DPRINTLN("closing connection");
-	//}
 }
 
 void setup()
 {
+  //wifiManager.resetSettings();  
 #ifdef DEBUG
 	Serial.begin(115200);
 #endif
-	dht.begin();
+	dht1.begin();
+	dht2.begin();
+	dht3.begin();
+	dht4.begin();
 	//read configuration from FS json
 	DPRINTLN("mounting FS...");
 	if (SPIFFS.begin())
@@ -128,9 +169,7 @@ void setup()
 				json.printTo(Serial);
 				if (json.success())
 				{
-					DPRINTLN("\nparsed json");
-					strcpy(thingspeakApiUrl, json["thingspeakApiUrl"]);
-					strcpy(thingspeakParameters, json["thingspeakParameters"]);
+					DPRINTLN("\nparsed json");				
 				}
 				else
 				{
@@ -143,27 +182,17 @@ void setup()
 	{
 		DPRINTLN("failed to mount FS");
 	}
-	DPRINTLN(thingspeakApiUrl);
-	DPRINTLN(thingspeakParameters);
-
-	WiFiManagerParameter custom_thingspeak_api_url("server", "thingspeakApiUrl", thingspeakApiUrl, 19);
-	WiFiManagerParameter custom_thingspeak_parameters("params", "thingspeakParameters", thingspeakParameters, 61);
-	wifiManager.addParameter(&custom_thingspeak_api_url);
-	wifiManager.addParameter(&custom_thingspeak_parameters);
+	
 	wifiManager.setSaveConfigCallback(saveConfigCallback);
 	wifiManager.autoConnect("ESP8266AP");
 	//if you get here you have connected to the WiFi
 	DPRINTLN("connected...yeey :)");
-	strcpy(thingspeakApiUrl, custom_thingspeak_api_url.getValue());
-	strcpy(thingspeakParameters, custom_thingspeak_parameters.getValue());
-
+	
 	if (shouldSaveConfig)
 	{
 		DPRINTLN("saving config");
 		DynamicJsonBuffer jsonBuffer;
 		JsonObject& json = jsonBuffer.createObject();
-		json["thingspeakApiUrl"] = thingspeakApiUrl;
-		json["thingspeakParameters"] = thingspeakParameters;
 		json["ip"] = WiFi.localIP().toString();
 		json["gateway"] = WiFi.gatewayIP().toString();
 		json["subnet"] = WiFi.subnetMask().toString();
